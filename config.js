02d2db241bb2924ea6f0708d50014577111cdbf4
sophie <sophie@waltonsmith@gmail.com>
2018-09-12 14:02:49 +1200

    Firebase initialised

---
+++ config.js
@@ -0,0 +1,14 @@
+
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBNPcD47E7dfNa4bGaGZmEsQcWfsSsOc50",
+    authDomain: "pin-point-app.firebaseapp.com",
+    databaseURL: "https://pin-point-app.firebaseio.com",
+    projectId: "pin-point-app",
+    storageBucket: "pin-point-app.appspot.com",
+    messagingSenderId: "451086133233"
+};
+
+firebase.initializeApp(firebaseConfig);
