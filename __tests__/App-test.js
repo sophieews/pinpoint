9e8c2828d3e20ce3e257eb7564511355def30470
sophie <sophie@waltonsmith@gmail.com>
2018-10-02 03:14:49 +1300

    Removed unused components and commented out tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,19 +4,21 @@ import App from '../App';
 import renderer from 'react-test-renderer';
 import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
 
-describe('App snapshot', () => {
-  jest.useFakeTimers();
-  beforeEach(() => {
-    NavigationTestUtils.resetInternalState();
-  });
+// Default test that came with CRNA, perhaps use this as an example in future to write tests
 
-  it('renders the loading screen', async () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders the root without loading screen', async () => {
-    const tree = renderer.create(<App skipLoadingScreen />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
+// describe('App snapshot', () => {
+//   jest.useFakeTimers();
+//   beforeEach(() => {
+//     NavigationTestUtils.resetInternalState();
+//   });
+//
+//   it('renders the loading screen', async () => {
+//     const tree = renderer.create(<App />).toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+//
+//   it('renders the root without loading screen', async () => {
+//     const tree = renderer.create(<App skipLoadingScreen />).toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -3,8 +3,11 @@ import React from 'react';
 import { MonoText } from '../StyledText';
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+// Default test that came with CRNA, perhaps use this as an example in future to write tests
 
-  expect(tree).toMatchSnapshot();
-});
+
+// it('renders correctly', () => {
+//   const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+//
+//   expect(tree).toMatchSnapshot();
+// });
